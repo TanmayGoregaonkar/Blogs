4051fa2b78e41893d25ca7951bc406cb53fe37a1
TanmayGoregaonkar <93464019+TanmayGoregaonkar@users.noreply.github.com>
2023-02-13 14:59:34 +0530

    IC

---
+++ api/index.js
@@ -0,0 +1,78 @@
+const express = require('express')
+const app = express();
+const cors = require('cors');
+const User = require('./models/User')
+const mongoose  = require('mongoose');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
+const multer = require('multer');
+const uploadMiddleware = multer({dest:'uploads/'});
+const fs = require('fs');
+
+const salt = bcrypt.genSaltSync(10);
+const secret = 'dshfejhjwg4hrjhhjdfve4jk4'
+
+app.use(cors({credentials:true,origin:'http://localhost:3000'}));
+app.use(express.json());
+app.use(cookieParser());
+
+mongoose.set('strictQuery', true);
+mongoose.connect('mongodb+srv://blog:Ji3zDWkXhsbt6BUP@cluster0.tcpsxwe.mongodb.net/?retryWrites=true&w=majority')
+
+app.post('/register',async(req,res)=>{
+    const {username,password} = req.body;
+    try{
+        const userDoc = await User.create({
+            username,
+            password:bcrypt.hashSync(password,salt)
+        });
+        res.json(userDoc);
+    }
+    catch(e){
+        res.status(400).json(e);
+    }
+});
+
+app.post('/login',async(req,res)=>{
+    const {username,password} = req.body;
+    const userDoc = await User.findOne({username});
+    const passOk = bcrypt.compareSync(password,userDoc.password);
+    if(passOk){
+        // logged in
+        jwt.sign({username,id:userDoc._id},secret,{},(err,token)=>{
+            if(err) throw err;
+            res.cookie('token',token).json({
+                id: userDoc._id,
+                username,
+            });
+        });
+
+    }else{
+        res.status(400).json('wrong credentials');
+    }
+});
+
+app.get('/profile',(req,res)=>{
+    const {token} = req.cookies;
+    jwt.verify(token,secret,{},(err,info)=>{
+        if(err) throw err;
+        res.json(info);
+    });
+   
+});
+
+app.post('/logout',(req,res)=>{
+    res.cookie('token','').json('ok');
+})
+
+app.post('/post',uploadMiddleware.single('file'),(req,res)=>{
+    const {originalname,path} = req.file;
+    const parts = originalname.split('.');
+    const ext = parts[parts.length-1];
+    const newPath = path+'.'+ext;
+    fs.renameSync(path,newPath);
+    res.json({ext});
+});
+app.listen(4000);
+
---
+++ client/src/Post.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+export default function Post  () {
+  return (
+    <div className="post">
+      <div className="image">
+        <img src="https://images5.alphacoders.com/111/1115011.png"></img>
+      </div>
+      <div className="texts">
+        <h2>Excited for season 3 - Kyujuro Rengoku is back ?</h2>
+        <p className="info">
+          <a className="author">Normal person</a>
+          <time>2023-01-07 13:12</time>
+        </p>
+        <p className="summary">
+          Reference site about Lorem Ipsum, giving information on its origins,
+          as well as a random Lipsum generator.
+        </p>
+      </div>
+      <div className="image">
+        <img src="https://i.pinimg.com/564x/4a/37/77/4a3777250830fb559f7cf0c8e4887c55.jpg"></img>
+      </div>
+      <div className="texts">
+        <h2>The Hashira`s Gathering - Can they bring down kokushibo & Muzan </h2>
+        <p className="info">
+          <a className="author">Normal person</a>
+          <time>2023-01-07 13:12</time>
+        </p>
+        <p className="summary">
+          Reference site about Lorem Ipsum, giving information on its origins,
+          as well as a random Lipsum generator.
+        </p>
+      </div>
+    </div>
+  );
+};
+
